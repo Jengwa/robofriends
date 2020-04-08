f579e9981166eda3693a01dcf5595d3b0fcd6535
Jengwa <sylvestertatenda@gmail.com>
2020-04-08 14:31:25 +0200

    Updates

---
+++ precache-manifest.79c1b8529f58ab76ea2a484701d11391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd42d1c9212571f3220df62b10dd435f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "16a3bcdeab24b5406974",
+    "url": "/robofriends/static/css/main.a736ed0f.chunk.css"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/robofriends/static/js/2.0bc7f1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0bc7f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a3bcdeab24b5406974",
+    "url": "/robofriends/static/js/main.95952511.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  }
+]);
